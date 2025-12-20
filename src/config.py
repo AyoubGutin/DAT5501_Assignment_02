8808fda47369fd16e80a5484ad4324b97cf6b0ec
Ayoub Gutin <gutinayoub@gmail.com>
2025-12-20 19:40:45 +0000

    Added config file for easier access to paths

---
+++ src/config.py
@@ -0,0 +1,17 @@
+# -- Imports --
+from pathlib import Path
+
+# Project Root
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
+
+# Folders
+DATA_DIR = PROJECT_ROOT / "data"
+RAW_DIR = DATA_DIR / "raw"
+PROCESSED_DIR = DATA_DIR / "processed"
+
+# Filenames
+DEMOGRAPHY_FILE = RAW_DIR / "business_demography_2024_ref_tables.xlsx"
+GVA_PATTERN = (
+    RAW_DIR / "regionalgrossvalueadded*.xlsx"
+)  # wildcard becuase of multiple files
+POPULATION_FILE = RAW_DIR / "populationestimatesbylocalauthority.xlsx"
