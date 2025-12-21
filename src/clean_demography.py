b1decfc27ce4e1bcda40e0c494085ebbee4ebac9
Ayoub Gutin <gutinayoub@gmail.com>
2025-12-21 15:33:29 +0000

    refactor(cleaning functions):
    - Noticed a trend within the population and business demo files, within
      the formatting (wide data)
    - As such, I rewrote the functions into a helper file, which can be
      reused within similar files, to avoid code duplication

--- src/clean_demography.py
+++ src/clean_demography.py
@@ -1,69 +1,6 @@
 from config import DEMOGRAPHY_FILE, PROCESSED_DIR
+from cleaning_helpers import clean_single_year, clean_multi_year, normalise_geo
 import pandas as pd
-
-
-def clean_single_year(sheet_name: str, value_name: str) -> pd.DataFrame:
-    """
-    Cleans excel sheets that are single years by region.
-    It reads a sheet where the third column header is the year and the values are under it
-
-    :param sheet_name: Sheet name of the excel file
-    :type sheet_name: str
-    :param year: Description
-    :type year: int
-    :return: Description
-    :rtype: DataFrame
-    """
-    df = pd.read_excel(
-        DEMOGRAPHY_FILE, sheet_name=sheet_name, header=3
-    )  # header is 3 as row 4 is where the data starts
-
-    geo_code_col = df.columns[0]
-    geo_name_col = df.columns[1]
-    year_col = df.columns[2]
-    year = int(year_col)
-
-    df = df.rename(
-        columns={
-            geo_code_col: "geo_code",
-            geo_name_col: "geo_name",
-            year_col: value_name,
-        }
-    )
-    df["year"] = year  # add the current year to a new column year
-
-    return df[["geo_code", "geo_name", "year", value_name]]
-
-
-def clean_multi_year(sheet_name: str, value_name: str) -> pd.DataFrame:
-    """
-    Read a sheet where columns 3+ are multiple years (e.g 2021, 2022, 2023)
-
-    :param sheet_name: Description
-    :type sheet_name: str
-    :param value_name: Description
-    """
-    df = pd.read_excel(DEMOGRAPHY_FILE, sheet_name=sheet_name, header=3)
-
-    df = df.rename(
-        columns={
-            df.columns[0]: "geo_code",
-            df.columns[1]: "geo_name",
-        }
-    )
-
-    years_cols = [c for c in df.columns if isinstance(c, int)]  # auto detect the years
-
-    # change the dataframe from multiple year columns to one year column, with the relevant births/deaths etc as a separate column
-    df = df.melt(
-        id_vars=["geo_code", "geo_name"],  # identifiers that repeat in every row
-        value_vars=years_cols,  # original year columns that hold birth counts, melt stacks into two new columns instead of separate ones
-        var_name="year",  # original column headers become values in a new column called year
-        value_name=value_name,  # the values undr the year columns become a single column
-    )
-    df["year"] = df["year"].astype(int)
-
-    return df[["geo_code", "geo_name", "year", value_name]]
 
 
 def build_births() -> pd.DataFrame:
@@ -73,16 +10,36 @@ def build_births() -> pd.DataFrame:
     :return: Description
     :rtype: DataFrame
     """
-    births_2019 = clean_single_year("Table 1.1a", "births")
-    births_2020 = clean_single_year("Table 1.1b", "births")
-    births_21_23 = clean_multi_year("Table 1.1c", "births")
-    births_2024 = clean_single_year("Table 1.1d", "births")
+    births_2019 = clean_single_year(
+        path_name=DEMOGRAPHY_FILE,
+        sheet_name="Table 1.1a",
+        header=3,
+        value_name="births",
+    )
+    births_2020 = clean_single_year(
+        path_name=DEMOGRAPHY_FILE,
+        sheet_name="Table 1.1b",
+        header=3,
+        value_name="births",
+    )
+    births_21_23 = clean_multi_year(
+        path_name=DEMOGRAPHY_FILE,
+        sheet_name="Table 1.1c",
+        header=3,
+        value_name="births",
+    )
+    births_2024 = clean_single_year(
+        path_name=DEMOGRAPHY_FILE,
+        sheet_name="Table 1.1d",
+        header=3,
+        value_name="births",
+    )
 
     births_all = pd.concat(
         [births_2019, births_2020, births_21_23, births_2024],
         ignore_index=True,
     )
-    births_all = _normalise_geo(births_all)
+    births_all = normalise_geo(births_all)
     return births_all
 
 
@@ -93,16 +50,36 @@ def build_deaths() -> pd.DataFrame:
     :return: Description
     :rtype: DataFrame
     """
-    deaths_2019 = clean_single_year("Table 2.1a", "deaths")
-    deaths_2020 = clean_single_year("Table 2.1b", "deaths")
-    deaths_21_23 = clean_multi_year("Table 2.1c", "deaths")
-    deaths_2024 = clean_single_year("Table 2.1d", "deaths")
+    deaths_2019 = clean_single_year(
+        path_name=DEMOGRAPHY_FILE,
+        sheet_name="Table 2.1a",
+        header=3,
+        value_name="deaths",
+    )
+    deaths_2020 = clean_single_year(
+        path_name=DEMOGRAPHY_FILE,
+        sheet_name="Table 2.1b",
+        header=3,
+        value_name="deaths",
+    )
+    deaths_21_23 = clean_multi_year(
+        path_name=DEMOGRAPHY_FILE,
+        sheet_name="Table 2.1c",
+        header=3,
+        value_name="deaths",
+    )
+    deaths_2024 = clean_single_year(
+        path_name=DEMOGRAPHY_FILE,
+        sheet_name="Table 2.1d",
+        header=3,
+        value_name="deaths",
+    )
 
     deaths_all = pd.concat(
         [deaths_2019, deaths_2020, deaths_21_23, deaths_2024],
         ignore_index=True,
     )
-    deaths_all = _normalise_geo(deaths_all)
+    deaths_all = normalise_geo(deaths_all)
     return deaths_all
 
 
@@ -113,31 +90,37 @@ def build_active() -> pd.DataFrame:
     :return: Description
     :rtype: DataFrame
     """
-    active_2019 = clean_single_year("Table 3.1a", "active")
-    active_2020 = clean_single_year("Table 3.1b", "active")
-    active_21_23 = clean_multi_year("Table 3.1c", "active")
-    active_2024 = clean_single_year("Table 3.1d", "active")
+    active_2019 = clean_single_year(
+        path_name=DEMOGRAPHY_FILE,
+        sheet_name="Table 3.1a",
+        header=3,
+        value_name="active",
+    )
+    active_2020 = clean_single_year(
+        path_name=DEMOGRAPHY_FILE,
+        sheet_name="Table 3.1b",
+        header=3,
+        value_name="active",
+    )
+    active_21_23 = clean_multi_year(
+        path_name=DEMOGRAPHY_FILE,
+        sheet_name="Table 3.1c",
+        header=3,
+        value_name="active",
+    )
+    active_2024 = clean_single_year(
+        path_name=DEMOGRAPHY_FILE,
+        sheet_name="Table 3.1d",
+        header=3,
+        value_name="active",
+    )
 
     active_all = pd.concat(
         [active_2019, active_2020, active_21_23, active_2024],
         ignore_index=True,
     )
-    active_all = _normalise_geo(active_all)
+    active_all = normalise_geo(active_all)
     return active_all
-
-
-def _normalise_geo(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    Docstring for _normalise_geo - tdo
-
-    :param df: Description
-    :type df: pd.DataFrame
-    :return: Description
-    :rtype: DataFrame
-    """
-    df["geo_code"] = df["geo_code"].astype(str).str.strip()
-    df["geo_name"] = df["geo_name"].astype(str).str.strip()
-    return df
 
 
 def check_duplicates(df: pd.DataFrame) -> pd.DataFrame:
