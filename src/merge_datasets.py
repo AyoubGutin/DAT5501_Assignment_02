dacd754f0ff84e8b793d1664890d7c77d123eadc
Ayoub Gutin <gutinayoub@gmail.com>
2025-12-22 14:42:07 +0000

    Inline documentation done for merge_datasets

--- src/merge_datasets.py
+++ src/merge_datasets.py
@@ -3,17 +3,20 @@ import pandas as pd
 from config import PROCESSED_DIR
 
 
+# -- Functions --
 def merge_all_datasets():
     """
-    Docstring for merge_all_datasets - todo
+    Merges business demography, population, and GVA datasets into a final dataset for the years 2019-2023.
 
-    :return: Description
+    :return: None
     :rtype: DataFrame
     """
+    # Load datasets from csvs
     demography = pd.read_csv(PROCESSED_DIR / "business_demography_counts.csv")
     population = pd.read_csv(PROCESSED_DIR / "population.csv")
     gva = pd.read_csv(PROCESSED_DIR / "gva.csv")
 
+    # Merge datasets using left joins on geo_code and year, to retain all demography records
     merged = demography.merge(
         population[["geo_code", "year", "population", "is_unreliable"]],
         on=["geo_code", "year"],
@@ -23,8 +26,10 @@ def merge_all_datasets():
         gva[["geo_code", "year", "gva_million"]], on=["geo_code", "year"], how="left"
     )
 
+    # Filter for years 2019-2023
     merged = merged[merged["year"].between(2019, 2023)]
 
+    # Reorder columns
     merged = merged[
         [
             "geo_code",
@@ -39,6 +44,7 @@ def merge_all_datasets():
         ]
     ]
 
+    # Save final dataset to csv
     out_path = PROCESSED_DIR / "final_dataset.csv"
     merged.to_csv(out_path, index=False)
 
